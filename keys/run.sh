0a289bf1b5dfddabefac2ec5be7720b6f7b9fc1d
patrick <patrick.sterk+gitlan@proton.me>
2024-04-03 15:13:17 +0200

    added another pihole

--- keys/run.sh
+++ keys/run.sh
@@ -5,7 +5,7 @@ names=( $( grep -E '^Host\s{1}\w+$' config | sed -E 's/Host\s{1}//' | sed -z 's/
 for name in "${names[@]}"
 do
     # Generate key pair
-    file_name="id_ed25519_$name"
+    file_name="${PWD}/id_ed25519_$name"
     [[ ! -f "$file_name" ]] && ssh-keygen -f "$file_name" -t ed25519 -N ""
     # test connection
     ssh -F config -q -i "$file_name" "$name" exit
